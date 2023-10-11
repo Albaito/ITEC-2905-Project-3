b56fe0e3baf020823e45b73ba0fe4385521924ce
Albaito <stanfordtramaine44@gmail.com>
2023-10-11 11:48:52 -0500

    modified DB structure

--- weather_db.py
+++ weather_db.py
@@ -1,24 +1,54 @@
 import datetime
 import sqlite3
 
-class Weather:
+db = 'storage.sqlite'
+
+class Weather:  # Placeholder class
     def __init__(self, date, temp, id=None, ):
         id = id
         timestamp = date
         temperature = temp
 
-    def update_cache(self):
+    def update(self):
         if self.id:
             pass
         else:
             pass
 
+    def save(self):
+
+        insert_weather_sql = 'insert into weather (temperature, weather) VALUES (?, ?, ?)'
+
+        with sqlite3.connect('storage.sqlite') as con:
+            con.execute(insert_weather_sql, ())
 
 
+def create_tables():
+    create_table_cache_sql = 'create table if not exists cache (id integer primary key not null, date_retrieved timestamp)'
+    create_table_weather_sql = 'create table if not exists weather (id integer, minimum_temperature integer not null, maximum_temperature integer not null, foreign key (id) references cache(id) on delete cascade)'
+    create_table_POI_sql = 'create table if not exists point_of_interest (id integer, name text,  lattitude float not null, longtitude float not null, )'
+
+    try:
+        with sqlite3.connect(db) as con:
+            con.execute(create_table_cache_sql)
+            con.execute(create_table_weather_sql)
+    except sqlite3.DatabaseError:
+        pass # TODO: should raises an error
+    finally:
+        con.commit()
+        con.close()
     
-with sqlite3.connect('storage.sqlite') as con:
-    con.execute('create table if not exists weather (temperature int, weather text, location text)')
+create_tables()
 
+def delete_all_data():
+    delete_all_cache_sql = 'delete from cache'
+    delete_all_weather_sql = 'delete from weather'
+
+    with sqlite3.connect(db) as con:
+        con.executemany(delete_all_cache_sql, delete_all_weather_sql)
+        con.commit()
+        con.close()
+    
 
 def get_all_weather():
     ''' gathers all rows of data from the weather table and returns them as objects in a list '''
