51fa636f9369e49a9c786d4ede299bff3ff0acfc
kpalmateer <98238056+kpalmateer@users.noreply.github.com>
2023-10-13 00:33:43 -0500

    Improve climate_api module to accept simple inputs

--- api_requests/climate_api.py
+++ api_requests/climate_api.py
@@ -1,17 +1,63 @@
 import requests
-import os
+from datetime import datetime
+from datetime import timedelta
 from pprint import pprint
 
 climate_url = 'https://archive-api.open-meteo.com/v1/archive?'
+geocode_url = 'https://geocoding-api.open-meteo.com/v1/search'
 
-latitude = '44.96'
-longitude = '-93.2638'
-start_date = '2023-09-18'
-end_date = '2023-10-02'
+location = 'berlin'
+date_string = '2023-01-06'
 daily_data = 'temperature_2m_min,temperature_2m_max,temperature_2m_mean,precipitation_sum,precipitation_hours'
-timezone = 'America/Chicago'
 
-def request_climate(lat, long, start, end, daily, tz):
+# call geocode API to get detailed location info from a simple input like "berlin"
+def request_geocode(loc):
+    geocode_query = {'name': loc}
+    try:
+        geocode_response = requests.get(geocode_url, params=geocode_query)
+        geocode_response.raise_for_status()
+        return geocode_response.json()
+    
+    except Exception as e:
+        print(e)
+
+# extract latitude, longitude, and timezone from the geocode JSON
+def get_latitude(geo):
+    lat = geo['results'][0]['latitude']
+    return lat
+
+def get_longitude(geo):
+    long = geo['results'][0]['longitude']
+    return long
+
+def get_timezone(geo):
+    tz = geo['results'][0]['timezone']
+    return tz
+
+# convert date string to date object
+def get_date(date_string):
+    date_object = datetime.strptime(date_string, '%Y-%m-%d').date()
+    return date_object
+
+# calculate start and end date
+def get_start_date(date):
+    start_date = date - (timedelta(days = 368))
+    return start_date
+
+def get_end_date(date):
+    end_date = date - (timedelta(days = 362))
+    return end_date
+
+# call climate API with parameters pulled from location and date input
+def request_climate(loc, date_string):
+    geocode = request_geocode(loc)
+    lat = get_latitude(geocode)
+    long = get_longitude(geocode)
+    date = get_date(date_string)
+    start = get_start_date(date)
+    end = get_end_date(date)
+    daily = daily_data
+    tz = get_timezone(geocode)
     climate_query = {'latitude': lat, 'longitude': long, 'start_date': start, 'end_date': end, 'daily': daily, 'timezone': tz}
     try:
         climate_response = requests.get(climate_url, params=climate_query)
@@ -22,7 +68,8 @@ def request_climate(lat, long, start, end, daily, tz):
         print(e)
 
 def main():
-    response = request_climate(latitude, longitude, start_date, end_date, daily_data, timezone)
+    response = request_climate(location, date_string)
+
     pprint(response)
 
 main()
