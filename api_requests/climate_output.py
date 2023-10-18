afa51c55f9357c770c3821d7a850a72e91da8a72
kpalmateer <98238056+kpalmateer@users.noreply.github.com>
2023-10-18 17:09:31 -0500

    Process API responses

--- api_requests/climate_output.py
+++ api_requests/climate_output.py
@@ -1,9 +1,53 @@
 from climate_api import request_climate
 from pprint import pprint
+from class_defs import ClimateDay
 
-location = 'berlin'
-date = '2020-01-10'
+# location_string = 'berlin'
+# date_string = '2020-01-10'
 
-response = request_climate(location, date)
+# def get_response(location, date):
+#     response = request_climate(location, date)
+#     return response
 
-pprint(response)
+def get_precip_time_list(response):
+    precip_time_list = response['daily']['precipitation_hours']
+    return precip_time_list
+
+def get_precip_list(response):
+    precip_list = response['daily']['precipitation_sum']
+    return precip_list
+
+def get_high_list(response):
+    high_list = response['daily']['temperature_2m_max']
+    return high_list
+
+def get_low_list(response):
+    low_list = response['daily']['temperature_2m_min']
+    return low_list
+
+def compile_data(location, date):
+    response = request_climate(location, date)
+    day = 0
+    days = []
+    high = get_high_list(response)
+    low = get_low_list(response)
+    precip = get_precip_list(response)
+    precip_time = get_precip_time_list(response)
+    while day < len(high):
+        day_object = ClimateDay(high[day], low[day], precip[day], precip_time[day])
+        days.append(day_object)
+        day = day + 1
+    return days
+
+# def main():
+#     api_response = get_response(location_string, date_string)
+#     day_dict = compile_data(api_response)
+#     day_count = 0
+#     while day_count < len(day_dict):
+#         print('The high temp will be ' + str(day_dict['day' + str(day_count)].high))
+#         print('The low temp will be ' + str(day_dict['day' + str(day_count)].low))
+#         print('The precipitation amount will be ' + str(day_dict['day' + str(day_count)].precip))
+#         print('The preciptiation duration will be ' + str(day_dict['day' + str(day_count)].precip_time))
+#         day_count = day_count + 1
+
+# main()
--- api_requests/poi_output.py
+++ api_requests/poi_output.py
@@ -1,8 +1,58 @@
 from poi_api import request_poi
 from pprint import pprint
+from class_defs import PointOfInterest
 
 location = 'berlin'
 
-response = request_poi(location)
+# def get_response(location):
+#     response = request_poi(location)
+#     return response
 
-pprint(response)
+def get_name(response, count):
+    poi_name = response['features'][count]['properties']['datasource']['raw']['name:en']
+    return poi_name
+
+def get_city(response, count):
+    poi_city = response['features'][count]['properties']['city']
+    return poi_city
+
+def get_lat(response, count):
+    poi_lat = response['features'][count]['properties']['lat']
+    return poi_lat
+
+def get_long(response, count):
+    poi_long = response['features'][count]['properties']['lon']
+    return poi_long
+
+def compile_data(location):
+    response = request_poi(location)
+    response_list = response['features']
+    count = 0
+    pois = []
+    while count < len(response_list):
+        name = get_name(response, count)
+        city = get_city(response, count)
+        lat = get_lat(response, count)
+        long = get_long(response, count)
+        poi_object = PointOfInterest(name, city, lat, long)
+        pois.append(poi_object)
+        count = count + 1
+    return pois
+    
+def main():
+    poi_list = compile_data('berlin')
+    print(poi_list)
+    for i in poi_list:
+        print(i.name)
+        print(i.city)
+        print(i.lat)
+        print(i.long)
+
+main()
+
+
+
+# test_response = get_response(location)
+# test_name = get_name(test_response, 0)
+
+# print(test_name)
