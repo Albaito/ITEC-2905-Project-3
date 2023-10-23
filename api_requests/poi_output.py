cd05f4754149790ef2f3d0f4a5dc774e15ab761b
kpalmateer <98238056+kpalmateer@users.noreply.github.com>
2023-10-22 20:25:52 -0500

    Fix name:en KeyError on country inputs

--- api_requests/poi_output.py
+++ api_requests/poi_output.py
@@ -1,8 +1,38 @@
 from poi_api import request_poi
 from pprint import pprint
+from class_defs import PointOfInterest
 
-location = 'berlin'
+def get_name(response, count):
+    # not all POIs have a 'name:en' value, so this will pull 'name' if there is a KeyError
+    try: 
+        poi_name = response['features'][count]['properties']['datasource']['raw']['name:en']
+    except KeyError:
+        poi_name = response['features'][count]['properties']['datasource']['raw']['name']
+    return poi_name
 
-response = request_poi(location)
+def get_city(response, count):
+    poi_city = response['features'][count]['properties']['city']
+    return poi_city
 
-pprint(response)
+def get_lat(response, count):
+    poi_lat = response['features'][count]['properties']['lat']
+    return poi_lat
+
+def get_long(response, count):
+    poi_long = response['features'][count]['properties']['lon']
+    return poi_long
+
+def compile_poi_data(location):
+    response = request_poi(location)
+    response_list = response['features']
+    count = 0
+    pois = []
+    while count < len(response_list):
+        name = get_name(response, count)
+        city = get_city(response, count)
+        lat = get_lat(response, count)
+        long = get_long(response, count)
+        poi_object = PointOfInterest(name, city, lat, long)
+        pois.append(poi_object)
+        count = count + 1
+    return pois
