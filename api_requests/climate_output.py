16425e87ac9b429274fd22e67a5a217dceff97ff
Albaito <stanfordtramaine44@gmail.com>
2023-10-22 23:28:05 -0500

    Removed api_requests

--- api_requests/climate_output.py
+++
@@ -1,9 +0,0 @@
-from climate_api import request_climate
-from pprint import pprint
-
-location = 'berlin'
-date = '2020-01-10'
-
-response = request_climate(location, date)
-
-pprint(response)
--- api_requests/poi_output.py
+++
@@ -1,8 +0,0 @@
-from poi_api import request_poi
-from pprint import pprint
-
-location = 'berlin'
-
-response = request_poi(location)
-
-pprint(response)
