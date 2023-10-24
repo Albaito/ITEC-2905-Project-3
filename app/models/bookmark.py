f5b4df7a6aa5756bd2525f38741148173b703db3
ahaid <ahaidahaid@>
2023-10-24 18:55:07 -0500

    Updated the view bookmark file to display the information

--- app/models/bookmark.py
+++ app/models/bookmark.py
@@ -1,6 +1,6 @@
 from app import db
-
+from datetime import datetime
 class Bookmark(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     location = db.Column(db.String(255), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    date = db.Column(db.Date, nullable=False, default=datetime.utcnow().date())
