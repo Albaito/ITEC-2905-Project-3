28f8d3f726b5e1e714c23d0bf17b3fce0ca0fd68
ahaid <ahaidahaid@>
2023-10-25 09:04:31 -0500

    Updated result_screen files

--- app/routes/results_screen.py
+++ app/routes/results_screen.py
@@ -1,13 +1,25 @@
 from app import app
 from flask import render_template, request
 from ..api_requests import *
+import json
 
 @app.route('/results_screen')
 def results_screen():
     location = request.args.get('location')
     date_str = request.args.get('date')
-    climate_data = climate_api.request_climate(location,date_str)
+    climate_data = climate_api.request_climate(location, date_str)
     pio_data = poi_api.request_poi(location)
-    youtube_videos=youtubeApiExample.city_video(location)
-    print(youtube_videos)
-    return render_template('results_screen.html',location=location, date=date_str,climate_data=climate_data,pio_data=pio_data,youtube_videos=youtube_videos)
+    youtube_videos_data = youtubeApiExample.city_video(location)
+    
+    if isinstance(youtube_videos_data, dict):
+        youtube_videos = [youtube_videos_data]
+    else:
+        try:
+            youtube_videos = json.loads(youtube_videos_data)
+        except json.JSONDecodeError:
+            print(f"Failed to decode JSON: {youtube_videos_data}")
+            youtube_videos = []
+
+    print(youtube_videos[0]['id'])
+
+    return render_template('results_screen.html', location=location, date=date_str, climate_data=climate_data, pio_data=pio_data, youtube_videos=youtube_videos)
